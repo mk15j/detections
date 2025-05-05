e50a0951a7056cc82366a9bacffc3be43bf7d5a5
mk15j <161435539+mk15j@users.noreply.github.com>
2025-05-05 16:54:09 +0530

    Update 0_Login.py

--- pages/0_Login.py
+++ pages/0_Login.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from utils.auth import authenticate  # Make sure this path is correct
+from streamlit.source_util import get_pages
 
+pages = get_pages("app.py")  # Replace with your actual main file name if different
+st.write("Available pages:", list(pages.keys()))
+for key, page in pages.items():
+    st.write(f"{page['page_name']}")
 st.title("🔐 Login")
 
 username = st.text_input("Username")
