14365cf2086c07ce7c5770c577288912ef9ed078
mk15j <161435539+mk15j@users.noreply.github.com>
2025-05-06 11:05:03 +0530

    Update 2_Trend_Analysis.py

--- pages/2_Trend_Analysis.py
+++ pages/2_Trend_Analysis.py
@@ -95,80 +95,6 @@ for bpdp in summary['before_during'].unique():
 
 
 
-# # Compute detection stats by week (without categorizing by before_during)
-# grouped = data.groupby(['week'])
-
-# summary = grouped['test_result'].agg(
-#     total_tests='count',
-#     detected_tests=lambda x: (x == 'Detected').sum()
-# ).reset_index()
-
-# summary['detection_rate_percent'] = (summary['detected_tests'] / summary['total_tests']) * 100
-
-# # Extract numeric part of week for proper sorting (e.g., "Week-12" → 12)
-# summary['week_num'] = summary['week'].str.extract(r'Week-(\d+)').astype(int)
-
-# # Sort by the extracted week number
-# summary = summary.sort_values(by='week_num')
-
-# # Create a single combo chart for total tests, detected tests, and detection rate
-# st.subheader("Detection Summary")
-
-# fig = go.Figure()
-
-# # Bar for total tests (Column chart)
-# fig.add_trace(go.Bar(
-#     x=summary['week'],
-#     y=summary['total_tests'],
-#     name='Total Tests',
-#     marker_color='skyblue',
-#     yaxis='y1',
-#     width=0.3
-# ))
-
-# # Bar for detected tests
-# fig.add_trace(go.Bar(
-#     x=summary['week'],
-#     y=summary['detected_tests'],
-#     name='Detected Tests',
-#     marker_color='orange',
-#     yaxis='y1',
-#     width=0.3
-# ))
-
-# # Line for detection rate % (Secondary y-axis)
-# fig.add_trace(go.Scatter(
-#     x=summary['week'],
-#     y=summary['detection_rate_percent'],
-#     name='Detection Rate (%)',
-#     mode='lines+markers',
-#     marker=dict(color='#C00000'),
-#     line=dict(color='#C00000'),
-#     yaxis='y2'
-# ))
-
-# # Layout with secondary y-axis and adjusted bar thickness
-# fig.update_layout(
-#     title="Detection Summary (All Production Phases)",
-#     # xaxis_title="Week",
-#     yaxis=dict(
-#         title="Total/Detected Tests",
-#         side="left",
-#         range=[0, 100]
-#     ),
-#     yaxis2=dict(
-#         title="Detection Rate (%)",
-#         overlaying="y",
-#         side="right",
-#         range=[0, 100]
-#     ),
-#     legend=dict(x=0.5, xanchor="center", orientation="h"),
-#     height=500,
-#     bargap=0.2,
-#     bargroupgap=0.1
-# )
-
-# st.plotly_chart(fig, use_container_width=True)
 
 
 import numpy as np
@@ -209,7 +135,7 @@ fig.add_trace(go.Bar(
     name='Total Tests',
     marker_color='#bbdefb',
     yaxis='y1',
-    width=0.1
+    width=0.2
 ))
 
 # Bar for detected tests
@@ -219,7 +145,7 @@ fig.add_trace(go.Bar(
     name='Detected Tests',
     marker_color='#42a5f5',
     yaxis='y1',
-    width=0.1
+    width=0.2
 ))
 
 # Line for detection rate %
